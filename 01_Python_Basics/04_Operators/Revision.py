682812eda8dad7d0fce72b2b6a9897a93bdc60c0
Gajanand-L-I <gajanandimmannavar@gmail.com>
2026-07-18 21:19:41 +0530

    Add Revision Problems on Operators

---
+++ 01_Python_Basics/04_Operators/Revision.py
@@ -0,0 +1,28 @@
+First_Number = 10
+Second_Number = 12
+
+if First_Number >= Second_Number:
+    print("First number is greater")
+elif :
+    print("Second number is greater")
+else :
+    print("Both are equal")
+    
+    
+    
+    
+    
+Marks = 35
+Pass = 40
+
+if Marks >= 35:
+    print("Pass")
+else:
+    print("Fail")
+    
+    
+Age  = 15
+if Age < 18:
+    print("Not Eligible")
+else:
+    print("Eligible")
