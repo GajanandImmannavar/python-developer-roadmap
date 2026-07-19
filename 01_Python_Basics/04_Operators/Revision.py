9845976648f22f6e20122db790da7f050a52520c
Gajanand-L-I <gajanandimmannavar@gmail.com>
2026-07-19 12:04:39 +0530

    Revision Problems on All Operators

--- 01_Python_Basics/04_Operators/Revision.py
+++ 01_Python_Basics/04_Operators/Revision.py
@@ -1,28 +1,32 @@
-First_Number = 10
-Second_Number = 12
 
-if First_Number >= Second_Number:
-    print("First number is greater")
-elif :
-    print("Second number is greater")
-else :
-    print("Both are equal")
-    
-    
-    
-    
-    
-Marks = 35
-Pass = 40
+# Problem 1 — Total Seconds
 
-if Marks >= 35:
-    print("Pass")
-else:
-    print("Fail")
-    
-    
-Age  = 15
-if Age < 18:
-    print("Not Eligible")
-else:
-    print("Eligible")
+# Store:
+
+# Seconds = 5432
+
+# Print:
+
+# 1 Hours
+# 30 Minutes
+# 32 Seconds
+
+
+
+Seconds = 5432
+
+Hours = Seconds // 360
+Remaining =  Seconds % 360
+
+Minutes = Seconds // 60
+REMAINING = Seconds % 60
+
+Secs = Seconds // 60
+Left = Seconds % 60
+
+print(f"{Hours} Hours ")
+print(f"{Minutes} Minutes")
+print(f"{Secs} Seconds")
+
+
+
