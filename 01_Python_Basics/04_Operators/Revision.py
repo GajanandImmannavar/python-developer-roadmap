363a20a8e957a6b195be49c8c70c005e4820c4c8
Gajanand-L-I <gajanandimmannavar@gmail.com>
2026-07-19 13:08:02 +0530

    Convert Seconds to Day:Hours:Minutes:Seconds Using // and %

--- 01_Python_Basics/04_Operators/Revision.py
+++ 01_Python_Basics/04_Operators/Revision.py
@@ -28,3 +28,41 @@ print(f"{REMAINING} Seconds")
 
 
 
+
+# Minutes = 8432
+
+Minutes = 8432
+Hours = Minutes // 3600
+Remaining = Minutes % 3600
+Minutes = Remaining // 60
+Sec = Remaining % 60
+print(f"{Hours}:Hours {Minutes}:Minutes {Sec}:Seconds")
+
+
+# Time
+
+# Convert
+
+# 98765 seconds
+
+# into
+
+# Days
+# Hours
+# Minutes
+# Seconds
+
+Seconds = 98765
+
+Day = Seconds // 86400
+Remaining = Seconds % 86400
+
+Hours = Remaining // 3600
+Left = Remaining % 3600
+
+Minutes = Left // 60
+Second = Left % 60
+
+
+print(f"{Day}: Day {Hours}: Hour {Minutes}:Minutes {Second}: Seconds")
+
