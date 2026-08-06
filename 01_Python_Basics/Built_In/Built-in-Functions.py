79171cc67be8e3469d1e66b8099c87a3b796de82
Gajanand-L-I <gajanandimmannavar@gmail.com>
2026-08-06 23:25:49 +0530

    Add Bool() Function Problems

--- 01_Python_Basics/Built_In/Built-in-Functions.py
+++ 01_Python_Basics/Built_In/Built-in-Functions.py
@@ -1,1 +1,98 @@
 # print()
+
+# Num = 1
+# print(Num)
+
+
+
+# x = 1
+# y = x
+# print(y)
+# print(x)
+
+
+# Input 
+
+# Age = input("Enter Age: ")
+# print("Gajanand is  " + Age + " Years Old")
+
+
+# print("Age Is=" +str(25))
+
+
+Name =input("Enter Your Sweet Name: ")
+PetName = input("Enter Your Pet Name If You have ")
+Age = input("Enter You Age to make sure whether your eligible for ticket or not: ")
+
+print("About Candidate : " +Name+ "  and Pet Name is  " +PetName+ " and  " +Age+ " Years Old" )
+# print("Pet Name " +PetName)
+# print("Age is " +Age)
+
+
+# Type function
+
+a = 22
+print(type(a))
+
+b = str(a)
+
+print(type(b))
+
+Name = "Gajanand"
+Ages =[[1,2],[3,4],[5,6]]
+Usn = [1,2,3,4]
+dict ={"Name": "Gajanand", "Age": 22}
+Majority = ((1,2,3,4,5))
+print(type(Name))
+print(type(Ages))
+
+print(type(Usn))
+print(type(dict))
+print(type(Majority))
+
+
+
+# bool() function 
+
+x = 10
+print(bool(x))
+
+# Output: True
+# Because the integer is not zero, it returns True.
+
+
+
+
+Name = "Gajanand"
+print(bool(Name))
+
+# Output: True
+# Because the string is not empty, it returns True.
+
+
+Age = 12
+My = 13
+print(bool(Age>My))
+
+# Output: False
+# Because the condition is not satisfied, it returns False.
+
+
+list1 = [1,2,3,4,5]
+print(bool(list1))
+
+# Output: True
+# Because the list is not empty, it returns True.
+
+
+
+
+
+
+name =" "
+print(bool(name))
+# Output: True 
+# Because the string is not empty, it returns True. and Whitespaces are also considered as a character in Python. 
+# So, it returns True.
+
+
