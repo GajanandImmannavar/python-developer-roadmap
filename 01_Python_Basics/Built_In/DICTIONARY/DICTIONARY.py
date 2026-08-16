e0bf068b962bf637097377c50b3abc16d5aac6d5
GajanandImmannavar <gajanandimmannavar@gmail.com>
2026-08-17 01:52:20 +0530

    Add Problems on Dictionary

--- 01_Python_Basics/Built_In/DICTIONARY/DICTIONARY.py
+++ 01_Python_Basics/Built_In/DICTIONARY/DICTIONARY.py
@@ -4,3 +4,58 @@ Data = {
 }
 
 print(type(Data))
+print(Data["Name"])
+print(Data["Mobile_NO"])
+
+
+Personal_Data = {
+
+
+   "About": {
+        "Name": "Gajanand",
+        "Educaion": "BE",
+        "Stream": "AIML",
+        "Job": "Looking for Job",
+        "Salary": ""
+           },
+
+
+    "Family":{
+                "Parents": "Lingaraj  and Sunanda",
+                "Siblings":"Gireesh And Malati",
+                "Grand_Parents":"Ramappa and Gangavva"  
+            }
+
+}
+
+# print("-------Whole Content Of Dictionary--------\n")
+# print(f"{Personal_Data}\n")
+
+# print(f"{Personal_Data['About']}\n")
+
+
+# print(f"{Personal_Data['About']['Stream']}\n")
+
+# print(f"{Personal_Data['Family']['Parents']}\n")
+
+
+print(Personal_Data.get('About'))
+print(Personal_Data.get('About').get('Stream'))
+
+Salary = Personal_Data.get("About",{}).get("Salary",0)
+print(Salary)
+
+
+
+# Personal_Data["About"]["Address"]= "Dist Bagalakot Tq Mudhol 587313"
+
+print(Personal_Data.get("About"))
+
+
+
+Personal_Data["About"]["Address"] = "Dist: Bagalakot Tq: Mudhol ATPost: Ingalagi 587313"
+
+
+
+
+print(Personal_Data.get("About",{}).get("Address"))
