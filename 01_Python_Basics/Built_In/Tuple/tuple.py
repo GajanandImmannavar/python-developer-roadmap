63f399449b44886b842f7035567ff099116fea89
GajanandImmannavar <gajanandimmannavar@gmail.com>
2026-08-26 15:27:39 +0530

    Add tuple index and slicing

---
+++ 01_Python_Basics/Built_In/Tuple/tuple.py
@@ -0,0 +1,54 @@
+nums = (1,2,3,4,5,6,7)
+print(type(nums))
+
+# output tuple
+
+
+
+# indexing
+
+names = ('Hi','Hello','Bye')
+print(names[1]) # secod element
+
+print(names[2]) # third element of tuple 
+
+# negative  indexing
+
+Numbers = [1,2,3,4,5]
+print(Numbers[-1])
+# output 5
+
+print(Numbers[-5])
+# output 1
+
+# slicing
+usn = (1,2,3,4,5)
+print(usn[1:2])
+
+print(usn[:])
+
+# reverse tuple
+
+no = (11,22,33,44)
+print(no[::-1])
+
+# tupe creation 
+
+A = (1,2,3,4,5,6,7,8,9,10)
+print(type(A))
+
+# output tupe
+
+# But parentheses are not actually what make it a tuple.
+# The comma is important.
+
+# The One-Element Tuple Trap
+
+Num = (1)
+print(type(Num))
+
+# output int
+
+# why because (1)
+# is just parentheses around 1.
+
